00698b94e59b08a4c1fb3356e96e9b62507aea80
huipengly <huipengly@gmail.com>
2018-07-03 20:20:51 +0800

    例4.1中需要用到矩阵预算部分，还未完成

--- cpp/Example4_1_CFDLMFAC1.cpp
+++ cpp/Example4_1_CFDLMFAC1.cpp
@@ -17,6 +17,7 @@
 #include <vector>
 #include <cmath>
 using std::vector;
+using std::cout; using std::endl;
 
 int main()
 {
@@ -34,7 +35,7 @@ int main()
     vector<double> u{0, 0};         //!<控制器输入
     vector<double> du{0, 0};        //!<控制器输入导数
 
-    //!计算期望值
+    //!计算期望值，实际中不需要
     for (int i = 0; i != 1000; ++i)
     {
         if (i <= 300)
@@ -53,10 +54,47 @@ int main()
 
     vector<double> phi{2, 2};       //!<φ
     vector<double> a{0, 0};
+    vector<double> err{0, 0};
     for (int i = 2; i != 1000; ++i)
     {
         a.push_back(1 + round(i / 500));
-        phi.push_back()
+        phi.push_back(phi[i - 1] + eta * (y[i] - y[i - 1] - phi[i - 1] * du[i - 1]) * du[i - 1] / (mu + du[i - 1] * du[i - 1]));
+        
+        //if (fai(k,1)<10^(-5)) || ((du(k-1,1:nu)*du(k-1,1:nu)')^0.5<10^(-5))
+        // @todo 这个地方需要矩阵运算，有问题
+        if (phi[i] < 1e-5 || sqrt(du[i - 1] * du[i - 1]) < 1e-5)
+        {
+            phi[i] = 0.5;
+        }
+
+        if (nu == 1)
+        {
+            u.push_back(u[i - 1] + rho * phi[i] * (yd[i + 1] - y[i]) / (lambda + phi[i] * phi[i]));
+        }
+        else
+        {
+            // @todo 矩阵运算
+            // u(k) = u(k-1)+rou*fai(k,1)*(yd(k+1)-y(k)-fai(k,2:nu)*du(k-1,1:nu-1)')/(lamda+fai(k,1).^2); 
+            // u.push_back(u[i - 1] + rho * phi[i] * (yd[i + 1] - y(k) - phi[i] * du[i]))
+        }
+
+        // 根据模型计算输出值，实际中不需要
+        if (i < 500)
+        {
+            y.push_back(y[i] / (1 + pow(y[i], 2)) + pow(u[i], 3));
+        }
+        else
+        {
+            y.push_back((y[i] * y[i - 1] * y[i - 2] * u[i - 1] * (y[i - 2] - 1) + a[i] * u[i]) / (1 + pow(y[i - 1], 2) + pow(y[i - 2], 2)));
+        }
+        du.push_back(u[i] - u[i - 1]);
+
+        err.push_back(yd[i] - y[i]);
+    }
+
+    for (auto &yk : y)
+    {
+        cout << yk << endl;
     }
 
     return 0;
