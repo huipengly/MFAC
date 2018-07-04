8de11212fb5aaed26a287456b1d5d2bed6904fad
huipengly <huipengly@gmail.com>
2018-07-04 19:23:33 +0800

    写完cfdl_mfac的类，还未测试

---
+++ cpp/CFDL_MFAC/CFDL_MFAC/main.cpp
@@ -0,0 +1,6 @@
+#include "mfac.h"
+
+int main()
+{
+	return 0;
+}
---
+++ cpp/CFDL_MFAC/CFDL_MFAC/mfac.cpp
@@ -0,0 +1,47 @@
+#include "mfac.h"
+
+
+
+Mfac::Mfac(const double &eta, const double &mu, const double &rho, const double &lambda, const double &epsilon) :
+	eta(eta),
+	mu(mu), 
+	rho(rho),
+	lambda(lambda),
+	epsilon(epsilon),
+	y_pre(1),
+	u_pre(0),
+	u(0),
+	du(0),
+	du_pre(0),
+	phi_first(2),
+	phi_pre(phi_first)
+{
+}
+
+Mfac::~Mfac()
+{
+}
+
+double Mfac::mfac_out(double yd, double y)
+{
+	//更新期望值和实际输出值
+	// @todo:研究函数变量和成员函数同名时怎么办
+	this->yd = yd;
+	y_pre = this->y;
+	this->y = y;
+
+	phi = phi_pre + eta * du_pre  * (y - y_pre - phi_pre * du_pre)/ (mu + pow(du_pre, 2));
+
+	if (fabs(phi) < epsilon || fabs(du_pre) < epsilon)
+	{
+		phi = phi_first;
+	}
+
+	u = u_pre + rho * phi * (yd - y) / (lambda + pow(phi, 2));
+
+	phi_pre = phi;
+	du_pre = u - u_pre;
+	u_pre = u;
+
+	return u;
+}
