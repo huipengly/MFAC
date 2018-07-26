2e3354b3495b9404a4c531cc9d33de3a6f1f5e9e
huipengly <huipengly@gmail.com>
2018-07-26 09:17:06 +0800

    完成练习5.1

--- cpp/CFDL_MFAC/CFDL_MFAC/main.cpp
+++ cpp/CFDL_MFAC/CFDL_MFAC/main.cpp
@@ -7,62 +7,29 @@ using std::vector;
 
 int main()
 {
-	const double nu = 1;            //!< ν
-	const double eta = 1;           //!< η
-	const double mu = 1;            //!< μ
-	const double rho = 0.6;         //!< ρ
-	const double lambda = 0.1;      //!< λ
-	const double pi = 3.14159;      //!< Π
+    const int N = 1000;
+    double y[1001]{1, 0.5, 0}, yd[1000]{0}, u1[1000]{0}, u2[1000]{0};
 
-	const int N = 1000;
+    vector<double> u(2);
 
-	vector<double> yd;              //!< 期望输出
-	vector<double> y{ -1, 1, 0.5 };   //!< 实际输出，并初始化y(1), y(2), y(3)
-	vector<double> u{ 0, 0 };         //!< 控制器输入
-	vector<double> du{ 0, 0 };        //!< 控制器输入导数
+    for (int i = 0; i != 1000; ++i)
+    {
+		yd[i] = pow(-1, round((i + 1) / 100.0));
+    }
 
-									  //!计算期望值，实际中不需要
-	for (int i = 0; i != 1001; ++i)
-	{
-		if (i <= 299)
+    MfacMiso mfac;
+
+    for (int i = 1; i < 1000; ++i)
+    {
+		if (i == 49)
 		{
-			yd.push_back(0.5 * pow((-1), round((i + 1) / 100.0)));
+			double aa = 1;
 		}
-		else if (i > 299 && i <= 699)
-		{
-			yd.push_back(0.5 * sin((i + 1) * pi / 100.0) + 0.3 * cos((i + 1) * pi / 50.0));
-		}
-		else if (i > 699)
-		{
-			yd.push_back(0.5 * pow((-1), round((i + 1) / 100.0)));
-		}
-	}
+        u = mfac.out(yd[i + 1], y[i]);
+		u1[i] = u[0];
+		u2[i] = u[1];
+        y[i + 1] = (5 * y[i] + 2 * u1[i] - 3 * u2[i] * u2[i] + 2 * u1[i] * u1[i]) / (5 + u1[i] + 5 * u2[i]);
+    }
 
-	vector<double> phi{ 2, 2 };       //!< φ
-	vector<double> a{ 0, 0 };
-	vector<double> err{ 0, 0 };
-
-	MfacMiso mfac(eta, mu, rho, lambda, 1e-5);
-	for (int k = 2; k != 1000; ++k)
-	{
-		a.push_back(1 + round((k + 1) / 500.0));
-
-		//使用控制器计算输入值u
-		u.push_back(mfac.out(yd[k + 1], y[k]));
-
-		// 根据模型计算输出值，实际中不需要
-		if (k <= 499)
-		{
-			y.push_back(y[k] / (1 + pow(y[k], 2)) + pow(u[k], 3));
-		}
-		else
-		{
-			y.push_back((y[k] * y[k - 1] * y[k - 2] * u[k - 1] * (y[k - 2] - 1) + a[k] * u[k]) / (1 + pow(y[k - 1], 2) + pow(y[k - 2], 2)));
-		}
-		du.push_back(u[k] - u[k - 1]);
-
-		err.push_back(yd[k] - y[k]);
-	}
-
-	return 0;
+    return 0;
 }
--- cpp/CFDL_MFAC/CFDL_MFAC/mfac_miso.cpp
+++ cpp/CFDL_MFAC/CFDL_MFAC/mfac_miso.cpp
@@ -9,7 +9,7 @@ MfacMiso::MfacMiso() :
     eta_(1),
     mu_(1), 
     rho_(1),
-    lambda_(1),
+    lambda_(3),
     epsilon_(1e-5),
     M_(50)
 {
@@ -19,17 +19,17 @@ MfacMiso::MfacMiso() :
     phi1_.resize(2);
     phi2_.resize(2);
 
-    y_[0] = 0;
-    y_[1] = 0;
+    y_[0] = 1;
+    y_[1] = 0.5;
     
-    u1_[0] = 0;
-    u1_[1] = 0;
+    u1_[0] = 1;
+    u1_[1] = 1;
 
-    u2_[0] = 0;
-    u2_[1] = 0;
+    u2_[0] = 1;
+    u2_[1] = 1;
 
-    phi1_init_value_ = phi1_[0] = 0;
-    phi2_init_value_ = phi2_[0] = 0;
+    phi1_init_value_ = phi1_[0] = 0.5;
+    phi2_init_value_ = phi2_[0] = -0.2;
 }
 
 MfacMiso::~MfacMiso()
@@ -45,19 +45,25 @@ MfacMiso::~MfacMiso()
  */
 vector<double> MfacMiso::out(double yd, double y)
 {
-    y_[1] = y;
-
     double du1 = u1_[1] - u1_[0];
     double du2 = u2_[1] - u2_[0];
+
+    y_[0] = y_[1];
+    u1_[0] = u1_[1];
+    u2_[0] = u2_[1];
+    phi1_[0] = phi1_[1];
+    phi2_[0] = phi2_[1];
+
+    y_[1] = y;
 
     phi1_[1] = phi1_[0] + eta_ * (y_[1] - y_[0] - phi1_[0] * du1 - phi2_[0] * du2) * du1 / (mu_ + du1 * du1 + du2 * du2);
     phi2_[1] = phi2_[0] + eta_ * (y_[1] - y_[0] - phi1_[0] * du1 - phi2_[0] * du2) * du2 / (mu_ + du1 * du1 + du2 * du2);
 
-    if (abs(phi1_[1] < epsilon_) || abs(phi1_[1] > M_))
+    if (abs(phi1_[1]) < epsilon_ || abs(phi1_[1]) > M_)
     {
         phi1_[1] = phi1_init_value_;
     }
-    if (abs(phi2_[1] < epsilon_) || abs(phi2_[1] > M_))
+    if (abs(phi2_[1]) < epsilon_ || abs(phi2_[1]) > M_)
     {
         phi2_[1] = phi2_init_value_;
     }
@@ -67,11 +73,5 @@ vector<double> MfacMiso::out(double yd, double y)
 
     vector<double> ret_u;
     ret_u.push_back(u1_[1]); ret_u.push_back(u2_[1]);
-
-    y_[0] = y_[1];
-    u1_[0] = u1_[1];
-    u2_[0] = u2_[1];
-    phi1_[0] = phi1_[1];
-    phi2_[0] = phi2_[1];
     return ret_u;
 }
